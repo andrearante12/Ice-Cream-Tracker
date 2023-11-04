915f92d7e4953002162e610fe20d6d0aaf5cffe1
gilbertdyer2 <gibberspam@gmail.com>
2023-11-04 17:26:50 -0400

    leaderboard help for andre

--- leaderboardScript.js
+++ leaderboardScript.js
@@ -3,8 +3,10 @@ let names = ["j", "k", "l", "m"]
 
 var temp = 0;
 var tempStr = "";
+
+// sort scores
 for (let i = 0; i < scores.length - 1; i++) {
-    if (scores[i] > scores[i + 1]) {
+    if (scores[i] < scores[i + 1]) {
         // score sorting
         temp = scores[i];
         scores[i] = scores[i + 1];
@@ -17,7 +19,24 @@ for (let i = 0; i < scores.length - 1; i++) {
         i = -1;
     }
 }
+arrayList = document.getElementById('arrayList');
+
+scores.forEach (item => {
+    listItem = document.createElement('li');
+    listItem.textContent = item;
+    arrayList.appendChild(listItem);
+  });Container.append(displayListItems);
+
+
+arrayListNames = document.getElementById('arrayList2');
+
+  names.forEach (item => {
+    listItem = document.createElement('li');
+    listItem.textContent = item;
+    arrayListNames.appendChild(listItem);
+  });Container.append(displayListItems);
+  
 // Call the function to display the leaderboard when the page loads
 document.getElementById('box').innerHTML = scores;
-document.getElementById('box').innerHTML = names;
+document.getElementById('box2').innerHTML = names;
 
